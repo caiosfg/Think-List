67da0f04c3e0cf4c201940dab572c60679de876f
Caio Cesar <caio.sfg@gmail.com>
2021-08-17 23:50:39 -0300

    Alguns ajustes de layout

--- client/src/components/TaskGrid.vue
+++ client/src/components/TaskGrid.vue
@@ -1,15 +1,44 @@
 <template>
   <div class="tasks">
     <template>
-      <div v-for="task in tasks" :key="task.name">{{ task.name }}</div>
+      <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
     </template>
   </div> 
 </template>
 
 <script>
+import Task from './Task.vue'
+
 export default {
+  components: { Task },
   props:{
     tasks: { type: Array, required : true }
   },
 }
 </script>
+
+<style>
+  .task {
+    box-sizing: border-box;
+    width: 350px;
+    height: 150px;
+    padding: 10px;
+    border-radius: 8px;
+    font-size: 2rem;
+    font-weight: 300;
+    cursor:pointer;
+    user-select: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .pending {
+    border-left: 12px solid #B73229;
+    background-color: #F44336 ;
+  }
+  .done {
+    color: #DDD;
+    border-left: 12px solid #0A8F08;
+    background-color: #4c4 ;
+  }
+</style>
